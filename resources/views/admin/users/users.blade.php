cb6b43d9a6fa91444c732b3c1daae2440803645c
goyuquan <gyqzone@sina.com>
2016-01-27 14:34:53 +0000

    update user table interface

--- resources/views/admin/users/users.blade.php
+++ resources/views/admin/users/users.blade.php
@@ -3,9 +3,10 @@
 @section('style')
 
 <style media="screen">
-    table th {
-        text-align: center;
-    }
+table th,table td {
+    text-align: center;
+}
+
 </style>
 
 @endsection
@@ -48,101 +49,143 @@
         <span id="search" class="btn btn-ribbon" data-title="search"><i class="fa-search"></i> <span class="hidden-mobile">Search</span></span>
     </span> -->
 
-    </div>
-    <!-- END RIBBON -->
+</div>
+<!-- END RIBBON -->
 
-    <!-- MAIN CONTENT -->
-    <div id="content">
+<!-- MAIN CONTENT -->
+<div id="content">
 
-        <div class="row">
+    <div class="row">
 
-					<!-- col -->
-					<div class="col-xs-12 col-sm-3 col-md-4 col-lg-4">
-						<h1 class="page-title txt-color-blueDark"><!-- PAGE HEADER --><i class="fa-fw fa fa-file-o"></i> Other Pages <span>&gt;
-							Forum Layout </span></h1>
-					</div>
-					<!-- end col -->
+        <!-- col -->
+        <div class="col-xs-12 col-sm-3 col-md-4 col-lg-4">
+            <h1 class="page-title txt-color-blueDark"><!-- PAGE HEADER --><i class="fa-fw fa fa-file-o"></i> Other Pages <span>&gt;
+                Forum Layout </span></h1>
+            </div>
+            <!-- end col -->
 
-					<!-- right side of the page with the sparkline graphs -->
-					<!-- col -->
-					<div class="col-xs-12 col-sm-9 col-md-8 col-lg-8">
-						<!-- sparks -->
-						<ul id="sparks">
-							<li class="sparks-info">
-								<h5> 全部文章共计 <span class="txt-color-blue"><i class="fa fa-search-plus"></i>&nbsp;&nbsp;条</span></h5>
-							</li>
-							<li class="sparks-info">
-								<h5> Site Traffic <span class="txt-color-purple"><i class="fa fa-arrow-circle-up" title="Increased"></i>&nbsp;45%</span></h5>
-							</li>
-							<li class="sparks-info">
-								<h5> Site Orders <span class="txt-color-greenDark"><i class="fa fa-shopping-cart"></i>&nbsp;2447</span></h5>
-							</li>
-						</ul>
-						<!-- end sparks -->
-					</div>
-					<!-- end col -->
+            <!-- right side of the page with the sparkline graphs -->
+            <!-- col -->
+            <div class="col-xs-12 col-sm-9 col-md-8 col-lg-8">
+                <!-- sparks -->
+                <ul id="sparks">
+                    <li class="sparks-info">
+                        <h5> 全部文章共计 <span class="txt-color-blue"><i class="fa fa-search-plus"></i>&nbsp;&nbsp;条</span></h5>
+                    </li>
+                    <li class="sparks-info">
+                        <h5> Site Traffic <span class="txt-color-purple"><i class="fa fa-arrow-circle-up" title="Increased"></i>&nbsp;45%</span></h5>
+                    </li>
+                    <li class="sparks-info">
+                        <h5> Site Orders <span class="txt-color-greenDark"><i class="fa fa-shopping-cart"></i>&nbsp;2447</span></h5>
+                    </li>
+                </ul>
+                <!-- end sparks -->
+            </div>
+            <!-- end col -->
 
-				</div>
+        </div>
 
+        <div class="jarviswidget jarviswidget-color-blueDark" id="wid-id-erysy" data-widget-editbutton="false">
 
-        <div class="row">
-            @if (count($users) > 0)
-            <div class="col-sm-12">
+            <header>
+                <span class="widget-icon"> <i class="fa fa-table"></i> </span>
+                <h2>Column Filters </h2>
 
-                <div class="well">
+            </header>
 
-                    <table class="table table-striped table-forum smart-form table-hover text-left">
+            <!-- widget div-->
+            <div>
+
+                <!-- widget edit box -->
+                <div class="jarviswidget-editbox">
+                    <!-- This area used as dropdown edit box -->
+
+                </div>
+                <!-- end widget edit box -->
+
+                <!-- widget content -->
+                <div class="widget-body no-padding">
+                    @if (count($users) > 0)
+                    <table id="user_table" class="table table-striped table-bordered smart-form">
                         <thead>
                             <tr>
-                                <th style="width:20px;">
+                                <th>
                                     <label class="checkbox"> <input type="checkbox" name="checkbox-inline"> <i></i>
                                     </label>
                                 </th>
-                                <th style="width:20px;">ID</th>
-                                <th style="width:60px;">名称</th>
-                                <th style="width:60px;"> email </th>
-                                <th style="width:80px;">创建时间</th>
-                                <th style="width: 80px;">更新时间</th>
-                                <th class="hidden-xs hidden-sm" style="width: 50px;">操作</th>
+                                <th>ID</th>
+                                <th>用户名</th>
+                                <th>email</th>
+                                <th>创建时间</th>
+                                <th>更新时间</th>
+                                <th>操作</th>
+                            </tr>
+                            <tr class="second">
+                                <td>
+                                </td>
+                                <td>
+                                    <label class="input">
+                                        <input type="text" name="search_browser" value="Filter browsers" class="search_init">
+                                    </label>
+                                </td>
+                                <td>
+                                    <label class="input">
+                                        <input type="text" name="search_platform" value="Filter platforms" class="search_init">
+                                    </label>
+                                </td>
+                                <td>
+                                    <label class="input">
+                                        <input type="text" name="search_version" value="Filter versions" class="search_init">
+                                    </label>
+                                </td>
+                                <td>
+                                    <label class="input">
+                                        <input type="text" name="search_grade" value="Filter grades" class="search_init">
+                                    </label>
+                                </td>
+                                <td>
+                                    <label class="input">
+                                        <input type="text" name="search_grade" value="Filter grades" class="search_init">
+                                    </label>
+                                </td>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($users as $user)
-                            <tr>
+                            <tr class="odd gradeX">
                                 <td>
-                                    <label class="checkbox"> <input type="checkbox" name="checkbox-inline"> <i></i>
+                                    <label class="checkbox">
+                                        <input type="checkbox" name="checkbox-inline"> <i></i>
                                     </label>
                                 </td>
-                                <td class="text-center">{{ $user->id }}</td>
-                                <td class="text-center">{{ $user->name }} </td>
-                                <td class="text-center">{{ $user->email }} </td>
-                                <td class="text-center">{{ substr($user->created_at,0,10) }} </td>
-                                <td class="text-center">{{ substr($user->updated_at,0,10) }} </td>
+                                <td>{{ $user->id }}</td>
+                                <td>{{ $user->name }} </td>
+                                <td>{{ $user->email }} </td>
+                                <td>{{ substr($user->created_at,0,10) }} </td>
+                                <td>{{ substr($user->updated_at,0,10) }} </td>
                                 <td class="hidden-xs hidden-sm">
                                     <div class="btn-group">
-                                        <a href="/admin/user/{{ $user->id }}/edit"> <i class="fa fa-edit" style="font-size:24px;"></i>
-                                        </a>
-                                        <a href="/admin/user/{{ $user->id }}/destroy"> <i class="fa fa-times" style="font-size:24px;"></i>
-                                        </a>
-                                    </div>
-                                </td>
+                                    <a href="/admin/user/{{ $user->id }}/edit"> <i class="fa fa-edit" style="font-size:24px;"></i>
+                                    </a>
+                                    <a href="/admin/user/{{ $user->id }}/destroy"> <i class="fa fa-times" style="font-size:24px;"></i>
+                                    </a>
+                                </div>
+                            </td>
                             </tr>
-                            <!-- end TR -->
                             @endforeach
                         </tbody>
                     </table>
-
-                    <div class="dt-row dt-bottom-row">
-                        <div class="row text-center">
-                            <div class="dataTables_paginate paging_bootstrap_full">
-
-                            </div>
-                        </div>
-                    </div>
+                    @endif
                 </div>
+                <!-- end widget content -->
+
             </div>
-            @endif
+            <!-- end widget div -->
+
         </div>
+
+
+
 
     </div>
     <!-- END MAIN CONTENT -->
@@ -156,11 +199,75 @@
 @endsection
 
 @section('script')
+<script src="/js/plugin/datatables/jquery.dataTables-cust.min.js"></script>
+<script src="/js/plugin/datatables/ColReorder.min.js"></script>
+<script src="/js/plugin/datatables/FixedColumns.min.js"></script>
+<script src="/js/plugin/datatables/ColVis.min.js"></script>
+<script src="/js/plugin/datatables/ZeroClipboard.js"></script>
+<script src="/js/plugin/datatables/media/js/TableTools.min.js"></script>
+<script src="/js/plugin/datatables/DT_bootstrap.js"></script>
 
 <script type="text/javascript">
+$(function(){
+
     $("#aside_user").addClass("open");
     $("#aside_user_").show();
     $("#aside_user_list").addClass("active");
+
+
+    /* Add the events etc before DataTables hides a column */
+    $("#user_table thead input").keyup(function() {
+        oTable.fnFilter(this.value, oTable.oApi._fnVisibleToColumnIndex(oTable.fnSettings(), $("thead input").index(this)));
+    });
+
+    $("#user_table thead input").each(function(i) {
+        this.initVal = this.value;
+    });
+    $("#user_table thead input").focus(function() {
+        if (this.className == "search_init") {
+            this.className = "";
+            this.value = "";
+        }
+    });
+    $("#user_table thead input").blur(function(i) {
+        if (this.value == "") {
+            this.className = "search_init";
+            this.value = this.initVal;
+        }
+    });
+
+
+    var oTable = $('#user_table').dataTable({
+        "sDom" : "<'dt-top-row'><'dt-wrapper't><'dt-row dt-bottom-row'<'row'<'col-sm-6'i><'col-sm-6 text-right'p>>",
+        //"sDom" : "t<'row dt-wrapper'<'col-sm-6'i><'dt-row dt-bottom-row'<'row'<'col-sm-6'i><'col-sm-6 text-right'>>",
+        "oLanguage" : {
+            "sSearch" : "Search all columns:"
+        },
+        "bSortCellsTop" : true
+    });
+
+
+    /* TABLE TOOLS */
+    $('#datatable_tabletools').dataTable({
+        "sDom" : "<'dt-top-row'Tlf>r<'dt-wrapper't><'dt-row dt-bottom-row'<'row'<'col-sm-6'i><'col-sm-6 text-right'p>>",
+        "oTableTools" : {
+            "aButtons" : ["copy", "print", {
+                "sExtends" : "collection",
+                "sButtonText" : 'Save <span class="caret" />',
+                "aButtons" : ["csv", "xls", "pdf"]
+            }],
+            "sSwfPath" : "js/plugin/datatables/media/swf/copy_csv_xls_pdf.swf"
+        },
+        "fnInitComplete" : function(oSettings, json) {
+            $(this).closest('#dt_table_tools_wrapper').find('.DTTT.btn-group').addClass('table_tools_group').children('a.btn').each(function() {
+                $(this).addClass('btn-sm btn-default');
+            });
+        }
+    });
+    
+});
+
+
 </script>
 
 @endsection
