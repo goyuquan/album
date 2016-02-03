1fbba7f60375d9719a0b52dc538fd43b99b8ce94
goyuquan <gyqzone@sina.com>
2016-02-03 09:35:30 +0000

    home page work 1.1

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,11 +1,17 @@
-<div class="ui container">
+<div id="footer" class="ui container">
     <div class="ui one column grid">
-        <div id="footer" class="column">
+        <div class="column">
             <a href="#">dsfadsfdsaf</a>
             <a href="#">dsfadsfdsaf</a>
             <a href="#">dsfadsfdsaf</a>
             <a href="#">dsfadsfdsaf</a>
-            <hr>
+        </div>
+    </div>
+</div>
+<hr>
+<div id="copyright" class="ui container">
+    <div class="ui one column grid">
+        <div class="column">
             版权所有©2008-2015铁道部信息技术中心  中国铁道科学研究院
             京ICP备10009636号
         </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 @section('style')
 
 <link rel="stylesheet" href="/css/swiper.min.css" >
+<link rel="stylesheet" href="/css/homepage.css" >
 
 <style media="screen">
 
@@ -58,35 +59,92 @@
     <div class="swiper-button-prev"></div>
 </div>
 
-<div class="container spark-screen">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
 
-                <div class="panel-body">
-                    Your Application's Landing Page.
+    <h2 id="latest_head" class="ui horizontal divider header"><i class="bar photo icon"></i> Our Latsed album </h2>
+    <div id="latest" class="ui three column grid">
+        <div class="column">
+            <a href="#"><img src="/image/banner/hero-personalV4.jpg" alt=""/></a>
+        </div>
+
+        <div class="column">
+            <div class="ui link cards">
+                <div class="card">
+                    <div class="image">
+                        <img src="/image/banner/hero-personalV4.jpg">
+                    </div>
+                    <div class="content">
+                        <div class="header">Matt Giampietro</div>
+                        <div class="meta">
+                            <a>好友</a>
+                        </div>
+                        <div class="description">Matthew is an interior designer living in New York. </div>
+                    </div>
+                    <div class="extra content">
+                        <span class="right floated">2013年加入 </span>
+                        <span><i class="user icon"></i> 75 Friends </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="ui link cards">
+                <div class="card">
+                    <div class="image">
+                        <img src="/image/banner/hero-consumerV42.jpg">
+                    </div>
+                    <div class="content">
+                        <div class="header">Matt Giampietro</div>
+                        <div class="meta">
+                            <a>好友</a>
+                        </div>
+                        <div class="description">Matthew is an interior designer living in New York. </div>
+                    </div>
+                    <div class="extra content">
+                        <span class="right floated">2013年加入 </span>
+                        <span><i class="user icon"></i> 75 Friends </span>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+
+    <div class="" style="width:100%;height:200px;background:#34363a;">
+
+    </div>
+
+    <h2 id="category_head" class="ui horizontal divider header"><i class="bar image icon"></i> Our album category</h2>
+    <div id="category" class="ui two column centered grid">
+        <div class="column">
+            <div class="column">
+                <a href="#"><img src="/image/banner/hero-personalV4.jpg" alt=""/></a>
+            </div>
+        </div>
+        <div class="four column centered row">
+            <div class="column"></div>
+            <div class="column"></div>
+        </div>
+    </div>
+
+<br>
 @endsection
 
 @section('script')
 
 <script type="text/javascript" src="/js/swiper.min.js"> </script>
 <script type="text/javascript">
-    var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        slidesPerView: 'auto',
-        centeredSlides: true,
-        autoplay: 2500,
-        autoplayDisableOnInteraction: false,
-        paginationClickable: true,
-        spaceBetween: 20
-    });
+    window.onload = function(){
+        var swiper = new Swiper('.swiper-container', {
+            pagination: '.swiper-pagination',
+            nextButton: '.swiper-button-next',
+            prevButton: '.swiper-button-prev',
+            slidesPerView: 'auto',
+            centeredSlides: true,
+            autoplay: 2500,
+            autoplayDisableOnInteraction: false,
+            paginationClickable: true,
+            spaceBetween: 20
+        });
+    }
 </script>
 @endsection
